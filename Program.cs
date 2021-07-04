b178cc2292e2b0426393553e55266408ef842e4f
Sofia Romero <sofiaromero@Sofias-MacBook-Pro.local>
2021-07-04 15:04:29 -0300

    Local README to be pushed to github

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace local_demo_repo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+﻿using System;
+namespace local_demo_repo
+{
+	public class README
+	{
+		public README()
+		{
+		}
+	}
+}
